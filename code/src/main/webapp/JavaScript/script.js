d37ac4ab94e0fd8c5e5cd01d49a7ea7fd108f980
Nico <nicolas.pv.et@hotmail.com>
2020-06-22 15:07:03 +0000

    add class with hide and show functionality

--- code/src/main/webapp/JavaScript/script.js
+++ code/src/main/webapp/JavaScript/script.js
@@ -1,5 +1,28 @@
-function TransitionFromLandingToLogin() {
-    const background = document.getElementById("wrapper-background");
-    background.classList.add("login-background");
-    background.classList.remove("landing-background");
+class LandingPage {
+  constructor(){
+    this.background = document.getElementById("wrapper-background");
+  }
+
+  hide() {
+    this.background.classList.remove("landing-background");
+  }
 }
+
+class LoginPage {
+  constructor(){
+    this.background = document.getElementById("wrapper-background");
+  }
+
+  show() {
+    this.background.classList.add("login-background");
+  }
+}
+
+function TransitionFromLandingToLogin() {
+  history.pushState('', 'Login', '/login.html');
+  const landingPage = new LandingPage();
+  landingPage.hide();
+  const loginPage = new LoginPage();
+  loginPage.show();
+}
+
