25e26d3693d9bee8162e3d7538e00333cbc8c5ec
Nico <nicolas.pv.et@hotmail.com>
2020-06-22 17:11:00 +0000

    add controller class to javascript and initialize in index.html

--- code/src/main/webapp/JavaScript/script.js
+++ code/src/main/webapp/JavaScript/script.js
@@ -3,6 +3,10 @@ class LandingPage {
     this.background = document.getElementById("wrapper-background");
   }
 
+  show() {
+    // do nothing for now
+  }
+
   hide() {
     this.background.classList.remove("landing-background");
   }
@@ -16,13 +20,41 @@ class LoginPage {
   show() {
     this.background.classList.add("login-background");
   }
+
+  hide() {
+    // do nothing for now
+  }
 }
 
-function TransitionFromLandingToLogin() {
-  history.pushState('', 'Login', '/login.html');
-  const landingPage = new LandingPage();
-  landingPage.hide();
-  const loginPage = new LoginPage();
-  loginPage.show();
+class PageController {
+  constructor () {
+    this.landingPage = new LandingPage();
+    this.loginPage = new LoginPage();
+    this.currentlyShown = "none";
+  }
+
+  hideCurrentPage() {
+    if (this.currentlyShown != "none") {
+      this.currentlyShown.hide()
+    }
+  }
+
+  show(pageToShow) {
+    // Put the history.pushState here!
+    this.hideCurrentPage();
+    switch(pageToShow) {
+      case "landing":
+        history.pushState('', 'Online Contact Tracing', pageToShow);
+        this.landingPage.show();
+        this.currentlyShown = this.landingPage;
+        break;
+      case "login":
+        history.pushState('', 'Login', pageToShow);
+        this.loginPage.show();
+        this.currentlyShown = this.loginPage;
+        break;
+    }
+  }
 }
+
 
