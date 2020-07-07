f27cd16c8dccafe8bab3f2b2da327d94e82872aa
Nico <nicolas.pv.et@hotmail.com>
2020-07-07 21:34:07 +0000

    add new directories to reflect DD

---
+++ code/src/main/java/com/google/sps/servlets/redirects/LandingServlet.java
@@ -0,0 +1,16 @@
+package com.google.sps.servlets.redirects;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/landing")
+public class LandingServlet extends HttpServlet {
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    response.sendRedirect("/?page=landing");
+  }
+}
