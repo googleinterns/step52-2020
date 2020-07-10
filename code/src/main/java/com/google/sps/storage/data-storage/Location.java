a7ffa6f317f4a758c4a88f24fcf71e00d47b6e05
Nico <nicolas.pv.et@hotmail.com>
2020-07-10 18:36:13 +0000

    change Location and Place to use composition

--- code/src/main/java/com/google/sps/storage/data-storage/Location.java
+++ code/src/main/java/com/google/sps/storage/data-storage/Location.java
@@ -4,16 +4,17 @@ package com.google.sps.storage;
 * This class stores a location based on geographic coordinates and
 * the time interval when the user was present
 */
-public class Location extends TimeInterval {
+public class Location {
 
   // Location History provides geographic coordinates multiplied by 10^7 for more accuracy
   private final long longitudeE7;
   private final long latitudeE7;
+  private final TimeInterval timeInterval;
 
   public Location(long lng, long lat, long intervalStart, long intervalEnd) {
-    super(intervalStart, intervalEnd);
     longitudeE7 = lng;
     latitudeE7 = lat;
+    timeInterval = new TimeInterval(intervalStart, intervalEnd);
   }
 
   public long getLongitudeE7() {
--- code/src/main/java/com/google/sps/storage/data-storage/Place.java
+++ code/src/main/java/com/google/sps/storage/data-storage/Place.java
@@ -3,15 +3,16 @@ package com.google.sps.storage;
 /**
 * This class keeps track of a place's name and id
 */
-public class Place extends TimeInterval {
+public class Place {
 
   private final String nameOfPlace;
   private final String placeId;
+  private final TimeInterval timeInterval;
   
   public Place(String name, String id, long intervalStart, long intervalEnd) {
-    super(intervalStart, intervalEnd);
     nameOfPlace = name;
     placeId = id;
+    timeInterval = new TimeInterval(intervalStart, intervalEnd);
   }
 
   public String getName() {
