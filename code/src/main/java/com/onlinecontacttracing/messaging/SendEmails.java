6fb22dd6eb4a435924218a5ba8db585d96c07035
Cynthia Ma <cwjjma@gmail.com>
2020-07-14 12:31:17 +0000

    Setup sending messages to contacts on list of contacts

--- code/src/main/java/com/onlinecontacttracing/messaging/SendEmails.java
+++ code/src/main/java/com/onlinecontacttracing/messaging/SendEmails.java
@@ -3,13 +3,31 @@ package com.onlinecontacttracing.messaging;
 import com.onlinecontacttracing.messaging.Message;
 import com.onlinecontacttracing.messaging.SystemMessage;
 import com.onlinecontacttracing.messaging.LocalityResource;
+import com.onlinecontacttracing.messaging.MessagingSetup;
+import javax.mail.internet.MimeMessage;
 
 public class SendEmails {
 
   //get contacts list from objectify
   //create message
-  private String messageToSend;
+  //create instance of messaging setup and try to email
+  //store results in database
+  private String emailHeader;
+  private String emailBody;
+  private Message messageObject;
+  private ArrayList<PotentialContact> contactsList;
   
+  public SendEmails (SystemMessage systemMessage, String localityResource, CustomizableMessage customizableMessage, String emailHeader, String messageLanguage, ArrayList<PotentialContact> contactsList) {
+    messageObject = new Message(systemMessage, localityResource, customizableMessage);
+    emailBody = messageObject.compileMessage(messageLanguage);
+    this.emailHeader = emailHeader;
+    this.contactsList = contactsList;
+  }
 
+  public void sendEmailsOut ()
+    PotentialContact contact;
+    for(PotentialContact contact : contactsList) {
+      
+    }
 
 }
