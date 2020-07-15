6dc7403525b82f11bd4ade1d819ba2ebbac56aae
Nico <nicolas.pv.et@hotmail.com>
2020-07-15 21:55:04 +0000

    remove redundant imports

--- code/src/main/java/com/onlinecontacttracing/servlets/delete-data/DeletePositiveUserContactsServlet.java
+++ code/src/main/java/com/onlinecontacttracing/servlets/delete-data/DeletePositiveUserContactsServlet.java
@@ -5,8 +5,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.FileWriter;   // Import the FileWriter class
-import java.io.IOException;  // Import the IOException class to handle errors
 import java.util.logging.Logger;
 
 @WebServlet("/delete-old-positive-user-contacts")
