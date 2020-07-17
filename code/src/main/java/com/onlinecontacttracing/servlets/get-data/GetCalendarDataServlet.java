0cd7a13a0559a80bc05b482a3cf4391a344f7e72
Nico <nicolas.pv.et@hotmail.com>
2020-07-17 21:25:55 +0000

    code now compiles but is not tested

--- code/src/main/java/com/onlinecontacttracing/servlets/get-data/GetCalendarDataServlet.java
+++ code/src/main/java/com/onlinecontacttracing/servlets/get-data/GetCalendarDataServlet.java
@@ -1,14 +1,17 @@
 package com.onlinecontacttracing.servlets;
 
-import com.google.auth.Credentials;
-import com.google.auth.oauth2.GoogleCredentials;
-import com.google.auth.oauth2.AccessToken;
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken.Payload;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.client.util.DateTime;
-import com.google.api.client.util.store.FileDataStoreFactory;
+
 import com.google.api.services.calendar.Calendar;
 import com.google.api.services.calendar.CalendarScopes;
 import com.google.api.services.calendar.model.Event;
@@ -21,10 +24,8 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.security.GeneralSecurityException;
 import java.util.Collections;
 import java.util.List;
 
@@ -33,39 +34,40 @@ public class GetCalendarDataServlet extends HttpServlet {
 
   private static final String APPLICATION_NAME = "Google Calendar API Java Quickstart";
   private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+  private static final String TOKENS_DIRECTORY_PATH = "tokens";
+    private static final List<String> SCOPES = Collections.singletonList(CalendarScopes.CALENDAR_READONLY);
+  private static final String CREDENTIALS_FILE_PATH = "/credentials.json";
 
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
     try {
-      final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
-      GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier.Builder(HTTP_TRANSPORT, JSON_FACTORY)
-        .setAudience(Collections.singletonList("1080865471187-u1vse3ccv9te949244t9rngma01r226m"))
-        .build();
+        final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+        GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier.Builder(HTTP_TRANSPORT, JSON_FACTORY)
+          .setAudience(Collections.singletonList("1080865471187-u1vse3ccv9te949244t9rngma01r226m"))
+          .build();
 
-      String idTokenString = request.getParameter("idtoken");
+        String idTokenString = request.getParameter("idtoken");
 
-      GoogleIdToken idToken = verifier.verify(idTokenString);
+        GoogleIdToken idToken = verifier.verify(idTokenString);
 
-      if (idToken != null) {
-        Payload payload = idToken.getPayload();
-        String userId = payload.getSubject();
+        if (idToken != null) {
+            Payload payload = idToken.getPayload();
+            String userId = payload.getSubject();
 
-       
+            Credential credential = getCredentials(HTTP_TRANSPORT, userId);
 
-      
-        Calendar service = new Calendar.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT, userId))
-            .setApplicationName(APPLICATION_NAME)
-            .build();
+            Calendar service = new Calendar.Builder(HTTP_TRANSPORT, JSON_FACTORY, credential)
+              .setApplicationName(APPLICATION_NAME)
+              .build();
 
-
-        // List the next 10 events from the primary calendar.
+            // List the next 10 events from the primary calendar.
             DateTime now = new DateTime(System.currentTimeMillis());
             Events events = service.events().list("primary")
-                    .setMaxResults(10)
-                    .setTimeMin(now)
-                    .setOrderBy("startTime")
-                    .setSingleEvents(true)
-                    .execute();
+              .setMaxResults(10)
+              .setTimeMin(now)
+              .setOrderBy("startTime")
+              .setSingleEvents(true)
+              .execute();
 
 
 
@@ -83,7 +85,6 @@ public class GetCalendarDataServlet extends HttpServlet {
                 }
             }
         }
-      
     } catch (Exception e) {
         e.printStackTrace();
 
@@ -98,21 +99,15 @@ public class GetCalendarDataServlet extends HttpServlet {
      */
     private static Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT, String userId) throws IOException {
         // Load client secrets.
-        InputStream in = CalendarQuickstart.class.getResourceAsStream(CREDENTIALS_FILE_PATH);
+        InputStream in = GetCalendarDataServlet.class.getResourceAsStream(CREDENTIALS_FILE_PATH);//create a class authAPI config constants, have this as a string, eliminate input reader
         if (in == null) {
             throw new FileNotFoundException("Resource not found: " + CREDENTIALS_FILE_PATH);
         }
-        GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
 
-        // Build flow and trigger user authorization request.
-        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
-                HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
-                .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
-                .setAccessType("offline")
-                .build();
-        LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
-        
-        Credential credential = loadCredential(String userId);
+        GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
+      
+        Credential credential = new GoogleAuthorizationCodeFlow.Builder(
+               HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES).build().loadCredential(userId);
 
         if (credential == null) {
             //response.sendRedirect('/login-servlet')
