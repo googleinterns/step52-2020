0c5b8a1fc9edf96917701305a75a05970cae4280
Nico <nicolas.pv.et@hotmail.com>
2020-07-21 01:41:23 +0000

    moved delete function OldDataDeleter

--- code/src/main/java/com/onlinecontacttracing/servlets/delete-data/OldDataDeleter.java
+++ code/src/main/java/com/onlinecontacttracing/servlets/delete-data/OldDataDeleter.java
@@ -1,5 +1,11 @@
 package com.onlinecontacttracing.servlets;
 
+import static com.googlecode.objectify.ObjectifyService.ofy;
+import com.google.common.collect.Iterables;
+import com.googlecode.objectify.Key;
+import java.util.logging.Logger;
+import java.time.Instant;
+
 /**
 * This class stores the information needed to query old data from a given class and delete it.
 */
@@ -7,4 +13,32 @@ abstract class OldDataDeleter {
   public abstract Class getType();
   public abstract String getQueryString();
   public abstract long getMaxAge();
+
+  /**
+  * This method deletes old data.
+  * If the query was successful, the number of entries deleted will be logged.
+  * Otherwise the exception is caught and logged.
+  */
+  public void delete(Logger log) {
+
+    // Fetch keys of old data
+    Iterable<Key<?>> oldDataKeys = ofy().load().type(getType())
+      .filter(getQueryString(), Instant.now().getEpochSecond() - getMaxAge()).keys();
+
+    // Save data type class name to report in log
+    String oldDataTypeName = getType().getSimpleName() + " class";
+
+    try {
+      // Check how many entries will be deleted to report in log
+      int numberOfDataToDelete = Iterables.size(oldDataKeys);
+
+      ofy().delete().keys(oldDataKeys).now();
+
+      // Log number of entries deleted
+      log.info( "Deleted " + numberOfDataToDelete + " entries from the " + oldDataTypeName);
+
+    } catch(Exception e) {
+      log.info( "Deleting from the " + oldDataTypeName + " failed. The following error was found: " + e.toString());
+    }
+  }
 } 
--- code/src/main/java/com/onlinecontacttracing/servlets/delete-data/PositiveUserContactsDeleter.java
+++ code/src/main/java/com/onlinecontacttracing/servlets/delete-data/PositiveUserContactsDeleter.java
@@ -1,19 +1,14 @@
 package com.onlinecontacttracing.servlets;
 
-import static com.googlecode.objectify.ObjectifyService.ofy;
 import com.onlinecontacttracing.storage.PositiveUserContacts;
 import com.onlinecontacttracing.storage.Constants;
-import com.google.common.collect.Iterables;
-import com.googlecode.objectify.Key;
-import java.util.logging.Logger;
-import java.time.Instant;
 
 /**
 * This class deletes old positive user contacts
 */
 class PositiveUserContactsDeleter extends OldDataDeleter {
 
-  public Class getType(){
+  public Class getType() {
     return PositiveUserContacts.class;
   }
 
@@ -26,35 +21,7 @@ class PositiveUserContactsDeleter extends OldDataDeleter {
   * subtracts maximum time a class is allowed to persist in memory
   * from the current time by the 
   */
-  public long getMaxAge(){
-    return Instant.now().getEpochSecond() - Constants.POSITIVE_USER_DATA_MAX_TIME;
-  }
-
-  /**
-  * This method deletes old data.
-  * If the query was successful, the number of entries deleted will be logged.
-  * Otherwise the exception is caught and logged.
-  */
-  public void delete(Logger log) {
-
-    // Fetch keys of old data
-    Iterable<Key<?>> oldDataKeys = ofy().load().type(getType())
-      .filter(getQueryString(), getMaxAge()).keys();
-
-    // Save data type class name to report in log
-    String oldDataTypeName = getType().getSimpleName() + " class";
-
-    try {
-      // Check how many entries will be deleted to report in log
-      int numberOfDataToDelete = Iterables.size(oldDataKeys);
-
-      ofy().delete().keys(oldDataKeys).now();
-
-      // Log number of entries deleted
-      log.info( "Deleted " + numberOfDataToDelete + " entries from the " + oldDataTypeName);
-
-    } catch(Exception e) {
-      log.info( "Deleting from the " + oldDataTypeName + " failed. The following error was found: " + e.toString());
-    }
+  public long getMaxAge() {
+    return Constants.POSITIVE_USER_DATA_MAX_TIME;
   }
 }
