a83efe594c4c66456e2893dbd9f2ba157c78657d
Cynthia Ma <cwjjma@gmail.com>
2020-07-23 14:50:55 +0000

    Add folder for testing messaging

---
+++ code/src/test/java/com/onlinecontacttracing/messaging/ProfanityFlaggingFilterTest.java
@@ -0,0 +1,17 @@
+package com.onlinecontacttracing.messaging;
+
+import org.junit.Test;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+ 
+@RunWith(JUnit4.class)
+public final class ProfanityFlaggingFilterTest {
+
+  @Test
+  public void containsProfanityCheck() {
+    assertTrue(true);
+  }
+}
