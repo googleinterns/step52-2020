3eba196837e9aa9c3b18088dc8c3e7e8b397039c
Nico <nicolas.pv.et@hotmail.com>
2020-07-28 23:36:43 +0000

    add constant for two weeks in milli seconds

--- code/src/main/java/com/onlinecontacttracing/storage/data-storage/Constants.java
+++ code/src/main/java/com/onlinecontacttracing/storage/data-storage/Constants.java
@@ -7,6 +7,7 @@ public class Constants {
   final static long SIX_HOURS_IN_SECONDS = 21600L;
   final static long ONE_WEEK_IN_SECONDS = 604800L;
   final static long TWO_WEEKS_IN_SECONDS = 1209600L;
+  final static long TWO_WEEKS_IN_MILLI_SECONDS = 1209600000L;
 
   public final static long BUSINESS_CONTACT_COOLDOWN_SECONDS = ONE_WEEK_IN_SECONDS;
 
@@ -24,4 +25,5 @@ public class Constants {
   public final static long POSITIVE_USER_DATA_MAX_TIME = SIX_HOURS_IN_SECONDS;
   public final static long NOTIFICATION_BATCH_MAX_TIME = ONE_WEEK_IN_SECONDS;
 
+  public final static long CONTACTS_QUERY_TIME = TWO_WEEKS_IN_MILLI_SECONDS;
 }
