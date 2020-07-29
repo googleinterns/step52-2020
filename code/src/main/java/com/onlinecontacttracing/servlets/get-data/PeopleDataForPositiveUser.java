97c856ce8ba6b08a6cb78f0b25381d14c620cba1
Nico <nicolas.pv.et@hotmail.com>
2020-07-29 21:58:01 +0000

    People runnable now stores to objectify

--- code/src/main/java/com/onlinecontacttracing/servlets/get-data/PeopleDataForPositiveUser.java
+++ code/src/main/java/com/onlinecontacttracing/servlets/get-data/PeopleDataForPositiveUser.java
@@ -1,20 +1,28 @@
 package com.onlinecontacttracing.authentication;
 
 import com.google.api.client.auth.oauth2.Credential;
-import com.onlinecontacttracing.storage.PotentialContact;
-import java.util.ArrayList;
+import com.onlinecontacttracing.storage.PositiveUserContacts;
+import com.googlecode.objectify.Objectify;
 
 class PeopleDataForPositiveUser implements Runnable {
+  private final Objectify ofy;
+  private final String userId;
   private final Credential credential;
-  ArrayList<PotentialContact> contacts;
 
-  public PeopleDataForPositiveUser(Credential credential, ArrayList<PotentialContact> contacts) {
+  public PeopleDataForPositiveUser(Objectify ofy, String userId, Credential credential) {
+    this.ofy = ofy;
+    this.userId = userId;
     this.credential = credential;
-    this.contacts = contacts;
   }
 
   @Override
   public void run() {
-    // TODO Get contacts from people api
+    // Initiate objects to store information
+    PositiveUserContacts positiveUserContacts = new PositiveUserContacts(userId);
+
+    // TODO: add people api
+
+    // Store data or replace old data with newer data.
+    // ofy.save().entity(positiveUserContacts).now();
   }
 }
