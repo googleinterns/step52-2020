8abc8bdcca6ec89e64227ed3ef7fbf04c2c51f07
Nico <nicolas.pv.et@hotmail.com>
2020-07-29 15:56:51 +0000

    clean up code in runnable

--- code/src/main/java/com/onlinecontacttracing/servlets/get-data/CalendarDataForPositiveUser.java
+++ code/src/main/java/com/onlinecontacttracing/servlets/get-data/CalendarDataForPositiveUser.java
@@ -16,20 +16,23 @@ import java.util.List;
 import java.util.Collections;
 import java.util.Optional;
 import com.googlecode.objectify.Objectify;
+import java.util.logging.Logger;
 
 class CalendarDataForPositiveUser implements Runnable {
 
-  private static final String APPLICATION_NAME = "Google Calendar API Java Quickstart";
+  private static final String APPLICATION_NAME = "Get Calendar Data From Positive User";
   private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+  private static final String calendarType = "primary";
+  static final Logger log = Logger.getLogger(CalendarDataForPositiveUser.class.getName());
 
+  private final Objectify ofy;
   private final String userId;
   private final Credential credential;
-  private final Objectify ofy;
 
   public CalendarDataForPositiveUser(Objectify ofy, String userId, Credential credential) {
+    this.ofy = ofy;
     this.userId = userId;
     this.credential = credential;
-    this.ofy = ofy;
   }
 
   @Override
@@ -42,33 +45,33 @@ class CalendarDataForPositiveUser implements Runnable {
         .setApplicationName(APPLICATION_NAME)
         .build();
 
-      // Query events between now and the last two weeks
+      // Query events between now and the API_QUERY_TIME
       long currentTime = System.currentTimeMillis();
       DateTime now = new DateTime(currentTime);
-      DateTime twoWeeksAgo = new DateTime(currentTime-Constants.CONTACTS_QUERY_TIME);
-      Events events = service.events().list("primary")
-        .setTimeMin(twoWeeksAgo)
+      DateTime startOfContactsQueryWindow = new DateTime(currentTime - Constants.API_QUERY_TIME);
+      Events events = service.events().list(calendarType)
+        .setTimeMin(startOfContactsQueryWindow)
         .setTimeMax(now)
         .execute();
-      List<Event> items = events.getItems();
 
       // Initiate objects to store information
       PositiveUserContacts positiveUserContacts = new PositiveUserContacts(userId);
       
       // Iterate through events to extract contacts and places
-      for (Event event : items) {
-        Optional<List<EventAttendee>> attendees = Optional.ofNullable(event.getAttendees());
+      for (Event event : events.getItems()) {
+        List<EventAttendee> attendees = Optional.ofNullable(event.getAttendees()).orElse(Collections.emptyList());
 
-        for (EventAttendee attendee : attendees.orElse(Collections.emptyList())) {
+        for (EventAttendee attendee : attendees) {
           positiveUserContacts.add(attendee.getDisplayName(), attendee.getEmail());
         }
       }
 
       ofy.save().entity(positiveUserContacts).now();
+      // TODO: add positiveUserLocations
       
     } catch (Exception e) {
       e.printStackTrace();
-      // Not sure what to do here, I tried throwing the exception but then run() isn't overwritten
+      log.warning("An exception occurred: " + e.toString());
     }
   }
 }
