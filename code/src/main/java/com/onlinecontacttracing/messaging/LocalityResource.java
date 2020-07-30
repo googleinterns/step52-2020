8648a5e43f67e7b32ab219d2b5e713d598fa1282
Cynthia Ma <cynthiama@google.com>
2020-07-30 21:31:59 +0000

    git stash

--- code/src/main/java/com/onlinecontacttracing/messaging/LocalityResource.java
+++ code/src/main/java/com/onlinecontacttracing/messaging/LocalityResource.java
@@ -1,73 +1,77 @@
 package com.onlinecontacttracing.messaging;
 
 public enum LocalityResource implements HasEnglishTranslation, HasSpanishTranslation{
-  US("https://www.cdc.gov/coronavirus/2019-ncov/index.html", "Here's the national help link: ", "Aquí está el enlace de ayuda nacional: "),
-  ALABAMA("http://www.alabamapublichealth.gov/covid19/index.html", "Here's your state's help link: ", "Aquí está el enlace de ayuda de su estado: "),
-  ALASKA("https://covid19.alaska.gov/", "Here's your state's help link: ", "Aquí está el enlace de ayuda de su estado: "),
-  ARIZONA("https://www.azdhs.gov/preparedness/epidemiology-disease-control/infectious-disease-epidemiology/index.php#novel-coronavirus-home", "Here's your state's help link: ", "Aquí está el enlace de ayuda de su estado: "),
-  ARKANSAS("https://www.healthy.arkansas.gov/programs-services/topics/novel-coronavirus", "Here's your state's help link: ", "Aquí está el enlace de ayuda de su estado: "),
-  CALIFORNIA("https://covid19.ca.gov/", "Here's your state's help link: ", "Aquí está el enlace de ayuda de su estado: "),
-  COLORADO("https://covid19.colorado.gov/covid-19-in-colorado/about-covid-19/testing-for-covid-19", "Here's your state's help link: ", "Aquí está el enlace de ayuda de su estado: "),
-  CONNECTICUT("https://portal.ct.gov/Coronavirus", "Here's your state's help link: ", "Aquí está el enlace de ayuda de su estado: "),
-  DELAWARE("https://coronavirus.delaware.gov/", "Here's your state's help link: ", "Aquí está el enlace de ayuda de su estado: "),
-  FLORIDA("https://floridahealthcovid19.gov/", "Here's your state's help link: ", "Aquí está el enlace de ayuda de su estado: "),
-  GEORGIA("https://dph.georgia.gov/health-topics/coronavirus-covid-19", "Here's your state's help link: ", "Aquí está el enlace de ayuda de su estado: "),
-  HAWAII("https://health.hawaii.gov/coronavirusdisease2019/", "Here's your state's help link: ", "Aquí está el enlace de ayuda de su estado: "),
-  IDAHO("https://coronavirus.idaho.gov/", "Here's your state's help link: ", "Aquí está el enlace de ayuda de su estado: "),
-  ILLINOIS("https://coronavirus.illinois.gov/s/", "Here's your state's help link: ", "Aquí está el enlace de ayuda de su estado: "),
-  INDIANA("https://www.coronavirus.in.gov/", "Here's your state's help link: ", "Aquí está el enlace de ayuda de su estado: "),
-  IOWA("https://coronavirus.iowa.gov/", "Here's your state's help link: ", "Aquí está el enlace de ayuda de su estado: "),
-  KANSAS("https://www.coronavirus.kdheks.gov/", "Here's your state's help link: ", "Aquí está el enlace de ayuda de su estado: "),
-  KENTUCKY("https://govstatus.egov.com/kycovid19", "Here's your state's help link: ", "Aquí está el enlace de ayuda de su estado: "),
-  LOUISIANA("http://ldh.la.gov/Coronavirus/", "Here's your state's help link: ", "Aquí está el enlace de ayuda de su estado: "),
-  MAINE("https://www.maine.gov/covid19/", "Here's your state's help link: ", "Aquí está el enlace de ayuda de su estado: "),
-  MARYLAND("https://coronavirus.maryland.gov/", "Here's your state's help link: ", "Aquí está el enlace de ayuda de su estado: "),
-  MASSACHUSETTS("https://www.mass.gov/info-details/covid-19-updates-and-information", "Here's your state's help link: ", "Aquí está el enlace de ayuda de su estado: "),
-  MICHIGAN("https://www.michigan.gov/coronavirus/0,9753,7-406-98163_98173---,00.html", "Here's your state's help link: ", "Aquí está el enlace de ayuda de su estado: "),
-  MINNESOTA("https://www.health.state.mn.us/diseases/coronavirus/", "Here's your state's help link: ", "Aquí está el enlace de ayuda de su estado: "),
-  MISSISSIPPI("https://msdh.ms.gov/msdhsite/_static/14,0,420.html", "Here's your state's help link: ", "Aquí está el enlace de ayuda de su estado: "),
-  MISSOURI("https://health.mo.gov/living/healthcondiseases/communicable/novel-coronavirus/", "Here's your state's help link: ", "Aquí está el enlace de ayuda de su estado: "),
-  MONTANA("https://dphhs.mt.gov/publichealth/cdepi/diseases/coronavirusmt", "Here's your state's help link: ", "Aquí está el enlace de ayuda de su estado: "),
-  NEBRASKA("http://dhhs.ne.gov/Pages/Coronavirus.aspx", "Here's your state's help link: ", "Aquí está el enlace de ayuda de su estado: "),
-  NEVADA("https://nvhealthresponse.nv.gov/", "Here's your state's help link: ", "Aquí está el enlace de ayuda de su estado: "),
-  NEW_HAMPSHIRE("https://www.nh.gov/covid19/", "Here's your state's help link: ", "Aquí está el enlace de ayuda de su estado: "),
-  NEW_JERSEY("https://covid19.nj.gov/", "Here's your state's help link: ", "Aquí está el enlace de ayuda de su estado: "),
-  NEW_MEXICO("https://cv.nmhealth.org/", "Here's your state's help link: ", "Aquí está el enlace de ayuda de su estado: "),
-  NEW_YORK("https://www1.nyc.gov/site/coronavirus/index.page", "Here's your state's help link: ", "Aquí está el enlace de ayuda de su estado: "),
-  NORTH_CAROLINA("https://covid19.ncdhhs.gov/dashboard", "Here's your state's help link: ", "Aquí está el enlace de ayuda de su estado: "),
-  NORTH_DAKOTA("https://www.health.nd.gov/diseases-conditions/coronavirus", "Here's your state's help link: ", "Aquí está el enlace de ayuda de su estado: "),
-  OHIO("https://coronavirus.ohio.gov/wps/portal/gov/covid-19/home", "Here's your state's help link: ", "Aquí está el enlace de ayuda de su estado: "),
-  OKLAHOMA("https://coronavirus.health.ok.gov/", "Here's your state's help link: ", "Aquí está el enlace de ayuda de su estado: "),
-  OREGON("https://govstatus.egov.com/OR-OHA-COVID-19", "Here's your state's help link: ", "Aquí está el enlace de ayuda de su estado: "),
-  PENNSYLVANIA("https://www.health.pa.gov/topics/disease/coronavirus/Pages/Coronavirus.aspx", "Here's your state's help link: ", "Aquí está el enlace de ayuda de su estado: "),
-  RHODE_ISLAND("https://health.ri.gov/covid/", "Here's your state's help link: ", "Aquí está el enlace de ayuda de su estado: "),
-  SOUTH_CAROLINA("https://www.scdhec.gov/infectious-diseases/viruses/coronavirus-disease-2019-covid-19", "Here's your state's help link: ", "Aquí está el enlace de ayuda de su estado: "),
-  SOUTH_DAKOTA("https://doh.sd.gov/news/Coronavirus.aspx", "Here's your state's help link: ", "Aquí está el enlace de ayuda de su estado: "),
-  TENNESSEE("https://www.tn.gov/governor/covid-19.html", "Here's your state's help link: ", "Aquí está el enlace de ayuda de su estado: "),
-  TEXAS("https://www.dshs.texas.gov/coronavirus/", "Here's your state's help link: ", "Aquí está el enlace de ayuda de su estado: "),
-  UTAH("https://coronavirus.utah.gov/", "Here's your state's help link: ", "Aquí está el enlace de ayuda de su estado: "),
-  VERMONT("https://www.healthvermont.gov/response/coronavirus-covid-19", "Here's your state's help link: ", "Aquí está el enlace de ayuda de su estado: "),
-  VIRGINIA("https://www.vdh.virginia.gov/coronavirus/", "Here's your state's help link: ", "Aquí está el enlace de ayuda de su estado: "),
-  WASHINGTON("https://www.doh.wa.gov/Emergencies/Coronavirus", "Here's your state's help link: ", "Aquí está el enlace de ayuda de su estado: "),
-  WEST_VIRGINIA("https://dhhr.wv.gov/COVID-19/Pages/default.aspx", "Here's your state's help link: ", "Aquí está el enlace de ayuda de su estado: "),
-  WISCONSIN("https://www.dhs.wisconsin.gov/covid-19/index.htm", "Here's your state's help link: ", "Aquí está el enlace de ayuda de su estado: "),
-  WYOMING("https://health.wyo.gov/publichealth/infectious-disease-epidemiology-unit/disease/novel-coronavirus/", "Here's your state's help link: ", "Aquí está el enlace de ayuda de su estado: ");
+  US("https://www.cdc.gov/coronavirus/2019-ncov/index.html"),
+  ALABAMA("http://www.alabamapublichealth.gov/covid19/index.html"),
+  ALASKA("https://covid19.alaska.gov/"),
+  ARIZONA("https://www.azdhs.gov/preparedness/epidemiology-disease-control/infectious-disease-epidemiology/index.php#novel-coronavirus-home"),
+  ARKANSAS("https://www.healthy.arkansas.gov/programs-services/topics/novel-coronavirus"),
+  CALIFORNIA("https://covid19.ca.gov/"),
+  COLORADO("https://covid19.colorado.gov/covid-19-in-colorado/about-covid-19/testing-for-covid-19"),
+  CONNECTICUT("https://portal.ct.gov/Coronavirus"),
+  DELAWARE("https://coronavirus.delaware.gov/"),
+  FLORIDA("https://floridahealthcovid19.gov/"),
+  GEORGIA("https://dph.georgia.gov/health-topics/coronavirus-covid-19"),
+  HAWAII("https://health.hawaii.gov/coronavirusdisease2019/"),
+  IDAHO("https://coronavirus.idaho.gov/"),
+  ILLINOIS("https://coronavirus.illinois.gov/s/"),
+  INDIANA("https://www.coronavirus.in.gov/"),
+  IOWA("https://coronavirus.iowa.gov/"),
+  KANSAS("https://www.coronavirus.kdheks.gov/"),
+  KENTUCKY("https://govstatus.egov.com/kycovid19"),
+  LOUISIANA("http://ldh.la.gov/Coronavirus/"),
+  MAINE("https://www.maine.gov/covid19/"),
+  MARYLAND("https://coronavirus.maryland.gov/"),
+  MASSACHUSETTS("https://www.mass.gov/info-details/covid-19-updates-and-information"),
+  MICHIGAN("https://www.michigan.gov/coronavirus/0,9753,7-406-98163_98173---,00.html"),
+  MINNESOTA("https://www.health.state.mn.us/diseases/coronavirus/"),
+  MISSISSIPPI("https://msdh.ms.gov/msdhsite/_static/14,0,420.html"),
+  MISSOURI("https://health.mo.gov/living/healthcondiseases/communicable/novel-coronavirus/"),
+  MONTANA("https://dphhs.mt.gov/publichealth/cdepi/diseases/coronavirusmt"),
+  NEBRASKA("http://dhhs.ne.gov/Pages/Coronavirus.aspx"),
+  NEVADA("https://nvhealthresponse.nv.gov/"),
+  NEW_HAMPSHIRE("https://www.nh.gov/covid19/"),
+  NEW_JERSEY("https://covid19.nj.gov/"),
+  NEW_MEXICO("https://cv.nmhealth.org/"),
+  NEW_YORK("https://www1.nyc.gov/site/coronavirus/index.page"),
+  NORTH_CAROLINA("https://covid19.ncdhhs.gov/dashboard"),
+  NORTH_DAKOTA("https://www.health.nd.gov/diseases-conditions/coronavirus"),
+  OHIO("https://coronavirus.ohio.gov/wps/portal/gov/covid-19/home"),
+  OKLAHOMA("https://coronavirus.health.ok.gov/"),
+  OREGON("https://govstatus.egov.com/OR-OHA-COVID-19"),
+  PENNSYLVANIA("https://www.health.pa.gov/topics/disease/coronavirus/Pages/Coronavirus.aspx"),
+  RHODE_ISLAND("https://health.ri.gov/covid/"),
+  SOUTH_CAROLINA("https://www.scdhec.gov/infectious-diseases/viruses/coronavirus-disease-2019-covid-19"),
+  SOUTH_DAKOTA("https://doh.sd.gov/news/Coronavirus.aspx"),
+  TENNESSEE("https://www.tn.gov/governor/covid-19.html"),
+  TEXAS("https://www.dshs.texas.gov/coronavirus/"),
+  UTAH("https://coronavirus.utah.gov/"),
+  VERMONT("https://www.healthvermont.gov/response/coronavirus-covid-19"),
+  VIRGINIA("https://www.vdh.virginia.gov/coronavirus/"),
+  WASHINGTON("https://www.doh.wa.gov/Emergencies/Coronavirus"),
+  WEST_VIRGINIA("https://dhhr.wv.gov/COVID-19/Pages/default.aspx"),
+  WISCONSIN("https://www.dhs.wisconsin.gov/covid-19/index.htm"),
+  WYOMING("https://health.wyo.gov/publichealth/infectious-disease-epidemiology-unit/disease/novel-coronavirus/");
   
   private String helpLink;
-  private final String englishTranslation;
-  private final String spanishTranslation;
+  private final String englishTranslation = "Here's your state's help link: ";
+  private final String spanishTranslation = "Aquí está el enlace de ayuda de su estado: ";
 
 
-  LocalityResource(String helpLink, String englishTranslation, String spanishTranslation) {
+  LocalityResource(String helpLink) {
     this.helpLink = helpLink;
-    this.englishTranslation = englishTranslation;
-    this.spanishTranslation = spanishTranslation;
   }
   public String getEnglishTranslation() {
-    return this.englishTranslation.concat(helpLink);
+    if (this == LocalityResource.US) {
+        return "Here's the national help link: ".concat(this.helpLink);
+    }
+    return this.englishTranslation.concat(this.helpLink);
   }
   public String getSpanishTranslation() {
-    return this.spanishTranslation.concat(helpLink);
+    if (this == LocalityResource.US) {
+        return "Aquí está el enlace de ayuda nacional: ".concat(this.helpLink);
+    }
+    return this.spanishTranslation.concat(this.helpLink);
   }
 
   public static LocalityResource getLocalityResourceFromString (String localityResourceName) {
