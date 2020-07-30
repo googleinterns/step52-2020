fc1b2568eabe76d4f2ad81e74491084e4f82ffc9
Nico <nicolas.pv.et@hotmail.com>
2020-07-30 23:03:56 +0000

    add getPlaceFromEvent method

--- code/src/main/java/com/onlinecontacttracing/storage/data-storage/PositiveUserPlaces.java
+++ code/src/main/java/com/onlinecontacttracing/storage/data-storage/PositiveUserPlaces.java
@@ -5,6 +5,13 @@ import com.googlecode.objectify.annotation.Id;
 import com.googlecode.objectify.annotation.Index;
 import java.time.Instant;
 import java.util.ArrayList;
+import com.google.maps.errors.ApiException;
+import com.google.api.services.calendar.model.Event;
+import com.google.maps.GeoApiContext;
+import java.io.IOException;
+import java.util.Optional;
+import com.google.maps.model.PlacesSearchResult;
+import com.google.maps.PlacesApi;
 
 /**
 * This class keeps track of a positive user's list of contacts from People and Calendar API
@@ -44,5 +51,21 @@ public class PositiveUserPlaces {
   @Override
   public String toString() {
     return String.format("The user (%s) has been to %s", userId, listOfPlaces.toString());
+  }
+
+  public void addPlaceFromEvent(Event event, GeoApiContext context) throws ApiException, InterruptedException, IOException {
+    Optional<String> addressOptional = Optional.ofNullable(event.getLocation());
+    
+    if (addressOptional.isPresent()) {
+      String address = addressOptional.get();
+      PlacesSearchResult[] results = PlacesApi.textSearchQuery(context, address).await().results;
+      if (results.length != 0) {
+        String placeId = results[0].placeId;
+        long startTimeSeconds = event.getStart().getDateTime().getValue() / 1000;
+        long endTimeSeconds = event.getEnd().getDateTime().getValue() / 1000;
+
+        add(placeId, address, startTimeSeconds, endTimeSeconds);
+      }
+    }
   }
 }
