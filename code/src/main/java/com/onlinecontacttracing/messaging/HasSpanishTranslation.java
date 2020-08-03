932a85be80bcdd301825156d1b0898ec058b912a
cynthia-m <45445464+cynthia-m@users.noreply.github.com>
2020-08-03 09:43:13 -0400

    Update HasSpanishTranslation.java

--- code/src/main/java/com/onlinecontacttracing/messaging/HasSpanishTranslation.java
+++ code/src/main/java/com/onlinecontacttracing/messaging/HasSpanishTranslation.java
@@ -1,5 +1,11 @@
 package com.onlinecontacttracing.messaging;
 
+/**
+* Guarantees a message has an English translation
+*/
 public interface HasSpanishTranslation {
+  /**
+  * Returns a message's English translation
+  */
   public String getSpanishTranslation();
 }
