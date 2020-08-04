8165d2a0f31640f972224e8e62ae0287e370dace
Josiah <jjoec321@gmail.com>
2020-08-04 04:17:07 +0000

    Pull Stroage files from master

--- code/src/main/java/com/onlinecontacttracing/storage/data-storage/PositiveUserPlaces.java
+++ code/src/main/java/com/onlinecontacttracing/storage/data-storage/PositiveUserPlaces.java
@@ -5,6 +5,13 @@ import com.googlecode.objectify.annotation.Id;
 import com.googlecode.objectify.annotation.Index;
 import java.time.Instant;
 import java.util.ArrayList;
+import com.google.maps.errors.ApiException;
+import com.google.api.services.calendar.model.Event;
+import com.google.maps.GeoApiContext;
+import java.io.IOException;
+import java.util.Optional;
+import com.google.maps.model.PlacesSearchResult;
+import com.google.maps.PlacesApi;
 
 /**
 * This class keeps track of a positive user's list of contacts from People and Calendar API
@@ -19,10 +26,10 @@ public class PositiveUserPlaces {
   // Objecify requires one constructor with no parameters
   private PositiveUserPlaces() {}
 
-  public PositiveUserPlaces(String id, ArrayList<Place> places) {
+  public PositiveUserPlaces(String id) {
     userId = id;
     timeCreatedSeconds = Instant.now().getEpochSecond();
-    listOfPlaces = places;
+    listOfPlaces = new ArrayList<Place>();
   }
 
   public String getUserId() {
@@ -37,4 +44,28 @@ public class PositiveUserPlaces {
     return timeCreatedSeconds;
   }
 
+  public void add(String placeId, String name, long intervalStart, long intervalEnd) {
+    listOfPlaces.add(new Place(placeId, name, intervalStart, intervalEnd));
+  }
+
+  @Override
+  public String toString() {
+    return String.format("The user (%s) has been to %s", userId, listOfPlaces.toString());
+  }
+
+  public void addPlaceFromEvent(Event event, GeoApiContext context) throws ApiException, InterruptedException, IOException {
+    Optional<String> addressOptional = Optional.ofNullable(event.getLocation());
+    
+    if (addressOptional.isPresent()) {
+      String address = addressOptional.get();
+      PlacesSearchResult[] results = PlacesApi.textSearchQuery(context, address).await().results;
+      if (results.length != 0) {
+        String placeId = results[0].placeId;
+        long startTimeSeconds = event.getStart().getDateTime().getValue() / 1000;
+        long endTimeSeconds = event.getEnd().getDateTime().getValue() / 1000;
+
+        add(placeId, address, startTimeSeconds, endTimeSeconds);
+      }
+    }
+  }
 }
